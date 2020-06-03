f0ebd195f3ab5d07124f09cd8274f21b9d6f65b8
typebrook <typebrook@gmail.com>
2020-06-03 14:14:09 +0800

    Fix logic error
    Steps:
    1. check page_(n).json exists
    2. if not exists, use variable 'url' as link. And store data into page_(n).json
    3. if exists, get link from page_(n).json. And store data into page_(n+1).json

--- scripts/generate-geojson.bash
+++ scripts/generate-geojson.bash
@@ -3,21 +3,23 @@ set -eo pipefail
 
 url='https://sta.ci.taiwan.gov.tw/STA_WaterResource_v2/v1.0/Datastreams?$expand=Thing,Thing/Locations,Observations($orderby=phenomenonTime+desc;$top=1)+&$filter=substringof(%27Datastream_Category_type=%E6%B7%B9%E6%B0%B4%E6%84%9F%E6%B8%AC%E5%99%A8%27,Datastreams/description)'
 key_next=@iot.nextLink
-mkdir -p docs/ raw/
+mkdir -p docs/ raw/ 
+rm raw/* || true
 count=1
 
+paged_file() { echo raw/page_$count.json; }
+
 while true; do
-  response=raw/page_$count.json
-  if [ -e $response ]; then 
-    link=$(jq -r .\"$key_next\" $response)
-    [[ $link == null ]] && break
+  if [ -e $(paged_file) ]; then 
+    link=$(jq -r .\"$key_next\" $(paged_file))
     ((count++))
+    [[ $link == null ]] && break
   else 
     link=$url
   fi
 
   curl --globoff $link \
-  | tee $response \
+  | tee $(paged_file) \
   | jq '.value[] |
     select(.Observations!=null) |
     .properties                   = .Thing.properties |
