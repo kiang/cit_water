94882ddd2975f584e8b28fda6dc0e8fedc7b97a1
kiang@h <kiang@osobiz.com>
2021-06-06 01:32:32 +0800

    fix commit branch

--- scripts/cron.php
+++ scripts/cron.php
@@ -11,4 +11,4 @@ exec("cd {$rootPath} && /usr/bin/git add -A");
 
 exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
 
-exec("cd {$rootPath} && /usr/bin/git push origin master");
+exec("cd {$rootPath} && /usr/bin/git push origin gh-pages");
